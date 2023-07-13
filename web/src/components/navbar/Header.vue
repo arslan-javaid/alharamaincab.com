bec92c68f42eb366e8ee695fc4ceb786f40193b4
hannanhaider <58129198+hannanhaider@users.noreply.github.com>
2023-07-13 08:34:32 +0500

    setup the file structure and added the coming soon page

---
+++ web/src/components/navbar/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container justify-content-center">
+      <!-- Logo -->
+      <a class="navbar-brand" href="#">
+        <img src="media/logos/logo.png" alt="Logo">
+      </a>
+    </div>
+  </nav>
+</template>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,31 +1,20 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import store from "@/store";
 import { Mutations, Actions } from "@/store/enums/StoreEnums";
 
 const routes: Array<RouteRecordRaw> = [
+  
   {
+    // the 404 route, when none of the above matches
     path: "/",
-    component: () => import("@/components/page-layouts/Auth.vue"),
-    children: [
-      {
-        path: "/sign-in",
-        name: "sign-in",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/SignIn.vue"),
-      },
-      {
-        path: "/sign-up",
-        name: "sign-up",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/SignUp.vue"),
-      },
-      {
-        path: "/password-reset",
-        name: "password-reset",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/PasswordReset.vue"),
-      },
-    ],
+    name: "home",
+    component: () => import("@/views/comingsoon/ComingSoonPage.vue"),
+  },
+  {
+    // the 404 route, when none of the above matches
+    path: "/about",
+    name: "about",
+    component: () => import("@/components/navbar/About.vue"),
   },
   {
     // the 404 route, when none of the above matches
@@ -45,7 +34,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
